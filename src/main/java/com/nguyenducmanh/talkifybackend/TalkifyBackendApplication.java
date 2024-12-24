1f095638da4672cb319d2315eb0d01746baf4a94
Manhduc13 <manhduc130702@gmail.com>
2024-12-24 14:00:53 +0700

    Step 1: Create project and config database connection

---
+++ src/main/java/com/nguyenducmanh/talkifybackend/TalkifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenducmanh.talkifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkifyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nguyenducmanh/talkifybackend/TalkifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenducmanh.talkifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
